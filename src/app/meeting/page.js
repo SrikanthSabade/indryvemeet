402e6bb3f231d368ed0d63e4c363e0212133c2b7
srikanth <srikanth@srikanth.sabade.local>
2024-10-03 13:06:01 +0530

    firt commit

--- src/app/meeting/page.js
+++ src/app/meeting/page.js
@@ -1,5 +1,5 @@
 "use client";
-import { useEffect, useState } from "react";
+import { Suspense, useEffect, useState } from "react";
 import { useRouter, useSearchParams } from "next/navigation";
 import { JaaSMeeting, JitsiMeeting } from "@jitsi/react-sdk";
 import { toast } from "react-toastify";
@@ -28,64 +28,66 @@ export default function Meeting() {
 
   return (
     <div className="bg-black">
-      {username ? (
-        <div className="bg-[#000000] h-[100vh] overflow-hidden">
-          <Large>
-            <JaaSMeeting
-              appId="vpaas-magic-cookie-a03a390d39ed4fd69d3be86f23ee08d6"
-              roomName={`${username}-meeting-room`} // Generate unique room for the user
-              configOverwrite={{
-                prejoinPageEnabled: false,
-              }}
-              interfaceConfigOverwrite={{
-                filmStripOnly: false,
-              }}
-              userInfo={{
-                displayName: username, // Show username in the meeting
-              }}
-              getIFrameRef={(iframeRef) => {
-                iframeRef.style.height = "100vh";
-              }}
-              onReadyToClose={() => {
-                toast.update("Meeting closed", {
-                  type: "success",
-                  isLoading: false,
-                  autoClose: 3000,
-                });
-                router.push("/");
-              }}
-            />
-          </Large>
-          <Small>
-            <JaaSMeeting
-              appId="vpaas-magic-cookie-a03a390d39ed4fd69d3be86f23ee08d6"
-              roomName={`${username}-meeting-room`} // Generate unique room for the user
-              configOverwrite={{
-                prejoinPageEnabled: false,
-              }}
-              interfaceConfigOverwrite={{
-                filmStripOnly: false,
-              }}
-              userInfo={{
-                displayName: username, // Show username in the meeting
-              }}
-              getIFrameRef={(iframeRef) => {
-                iframeRef.style.height = "90vh";
-              }}
-              onReadyToClose={() => {
-                toast.update("Meeting closed", {
-                  type: "success",
-                  isLoading: false,
-                  autoClose: 3000,
-                });
-                router.push("/");
-              }}
-            />
-          </Small>
-        </div>
-      ) : (
-        <p>Loading...</p>
-      )}
+      <Suspense fallback={<div>Loading...</div>}>
+        {username ? (
+          <div className="bg-[#000000] h-[100vh] overflow-hidden">
+            <Large>
+              <JaaSMeeting
+                appId="vpaas-magic-cookie-a03a390d39ed4fd69d3be86f23ee08d6"
+                roomName={`${username}-meeting-room`} // Generate unique room for the user
+                configOverwrite={{
+                  prejoinPageEnabled: false,
+                }}
+                interfaceConfigOverwrite={{
+                  filmStripOnly: false,
+                }}
+                userInfo={{
+                  displayName: username, // Show username in the meeting
+                }}
+                getIFrameRef={(iframeRef) => {
+                  iframeRef.style.height = "100vh";
+                }}
+                onReadyToClose={() => {
+                  toast.update("Meeting closed", {
+                    type: "success",
+                    isLoading: false,
+                    autoClose: 3000,
+                  });
+                  router.push("/");
+                }}
+              />
+            </Large>
+            <Small>
+              <JaaSMeeting
+                appId="vpaas-magic-cookie-a03a390d39ed4fd69d3be86f23ee08d6"
+                roomName={`${username}-meeting-room`} // Generate unique room for the user
+                configOverwrite={{
+                  prejoinPageEnabled: false,
+                }}
+                interfaceConfigOverwrite={{
+                  filmStripOnly: false,
+                }}
+                userInfo={{
+                  displayName: username, // Show username in the meeting
+                }}
+                getIFrameRef={(iframeRef) => {
+                  iframeRef.style.height = "90vh";
+                }}
+                onReadyToClose={() => {
+                  toast.update("Meeting closed", {
+                    type: "success",
+                    isLoading: false,
+                    autoClose: 3000,
+                  });
+                  router.push("/");
+                }}
+              />
+            </Small>
+          </div>
+        ) : (
+          <p>Loading...</p>
+        )}
+      </Suspense>
     </div>
   );
 }
